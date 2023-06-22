327fd1502f746a5147ec391d0d470a8cdcbbff95
Nagaraj <goudanagaraj789@gmail.com>
2023-06-22 12:45:25 +0530

    DOM

--- one.js
+++ one.js
@@ -1,6 +1,6 @@
 let user=[45,42,41,52]
-let new_user=user.map((user)=>{
+let new_user=user.forEach((user)=>{
     return user + 1;
 })
-console.log(new_user)
-console.log(user);
+console.log(new_user);
+console.log(user)
