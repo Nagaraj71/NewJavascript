43c03ba6f8f6e9f618414e0acc80bdb3ba070f54
Nagaraj <goudanagaraj789@gmail.com>
2023-06-22 16:40:53 +0530

    Ok

--- bcrypt.js
+++ bcrypt.js
@@ -1,12 +1,6 @@
-const bcrypt=require("bcryptjs")
-let user ={name:"nagaraj",email:"nagaraj@dilabs.in", password:"1234"};
-let salt= bcrypt.genSaltSync(10);
-let new_password = bcrypt.hashSync(user.password,salt);
-let new_user = {...user, new_password};
-
-let result= bcrypt.compareSync("1234",new_password);
-if(result){
-    console.log("Login Successfully");
-}else{
-    console.log("Login Failed");
-}
+const bcrypt= require("bcryptjs")
+let user={name:"nagaraj",email:"nagaraj@dilabs.in",password:"1234"}
+let salt =bcrypt.genSaltSync(10);
+let new_password= bcrypt.hashSync(user.password,salt)
+let new_user={...user,password:new_password}
+console.log(new_user);
--- one.js
+++
@@ -1,4 +0,0 @@
-function mani(){
-    setTimeout(function(){console.log("Good Evening"),2000})
-}mani();
-mani();
