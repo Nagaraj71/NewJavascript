d778b09ec9cb0faf3d3867c115f29309875aad6e
Nagaraj <goudanagaraj789@gmail.com>
2023-06-22 11:13:35 +0530

    Ok

--- bcrypt.js
+++ bcrypt.js
@@ -1,10 +1,10 @@
 const bcrypt=require("bcryptjs")
-let user ={name:"nagaraj",email:"nagaraj@dilabs.in", password:"1234"}
+let user ={name:"nagaraj",email:"nagaraj@dilabs.in", password:"1234"};
 let salt= bcrypt.genSaltSync(10);
-let new_password = bcrypt.hashSync(user.password,salt)
-let new_user = {...user, new_password}
+let new_password = bcrypt.hashSync(user.password,salt);
+let new_user = {...user, new_password};
 
-let result= bcrypt.compareSync("1234",new_password)
+let result= bcrypt.compareSync("1234",new_password);
 if(result){
     console.log("Login Successfully");
 }else{
