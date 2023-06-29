b400b5f6fc0b581e9b84f1b4afe6632cd0c26d8e
Nagaraj <goudanagaraj789@gmail.com>
2023-06-29 17:26:45 +0530

    Ok

--- one.js
+++ one.js
@@ -1,8 +1,1 @@
-const http=require('http')
-const server=http.createServer((req,res)=>{
-        Response.end("HTTP")
-})
-server.listen(2000,(err)=>{
-        if(err) throw err
-console.log("Server Running on Http")
-})
+const bcrypt=require("bcryptjs")
