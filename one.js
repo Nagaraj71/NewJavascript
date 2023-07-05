bb7d9d63c3e2ef3a3b6e7a2216ce25b48d826753
Nagaraj <goudanagaraj789@gmail.com>
2023-07-05 17:57:18 +0530

    token

--- one.js
+++ one.js
@@ -3,4 +3,9 @@ let user={email:"nagaraj@dilabs.in",password:"12345"}
 let payload={id:user.email};
 let secretkey="HEDFSOFA";
 let token=Jwt.sign(payload,secretkey,{expiresIn:60*60});
-console.log(token)
+console.log(token);
+
+Jwt.verify(token,secretkey,(err,new_payload)=>{
+if(err) throw err
+console.log(new_payload);
+})
