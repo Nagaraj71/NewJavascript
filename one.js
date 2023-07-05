1bffbc3b22226e7a2094e7d9f2ba5f2f8e4414fa
Nagaraj <goudanagaraj789@gmail.com>
2023-07-05 17:50:38 +0530

    Ok

--- one.js
+++ one.js
@@ -1,6 +1,6 @@
 const Jwt= require("jsonwebtoken")
 let user={email:"nagaraj@dilabs.in",password:"12345"}
-let payload={id:user.email}
+let payload={id:user.email};
 let secretkey="HEDFSOFA";
 let token=Jwt.sign(payload,secretkey,{expiresIn:60*60});
 console.log(token)
