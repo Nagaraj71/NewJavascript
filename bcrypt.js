ad3dc7554f6c4d876dd9b483ba212c0850f47eee
Nagaraj <goudanagaraj789@gmail.com>
2023-07-05 16:31:08 +0530

    Ok

--- bcrypt.js
+++
@@ -1,8 +0,0 @@
-const bcrypt=require("bcryptjs")
-let user={name:"nagaraj", email:"nagaraj@dilabs.in",password:"1234"}
-let salt=bcrypt.genSaltSync(10)
-let new_password=bcrypt.hashSync(user.password, salt)
-
-let new_user={...user, password:new_password}
-
-console.log(new_user)
