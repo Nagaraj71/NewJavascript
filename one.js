434abe72aa90e123a3c92a11dbbece1a4f5e102d
Nagaraj <goudanagaraj789@gmail.com>
2023-07-10 14:24:25 +0530

    Ok

--- one.js
+++ one.js
@@ -1,9 +1,18 @@
-const http=require('http')
-const server=http.createServer((req,res)=>{
-    response.end("http")
-})
-server.listen(3000,(err)=>{
-    if(err) throw err;
-    console.log("Server is running on the online")
-})
+const bcrypt=require("bcryptjs");
+let user={
+    name:"nagaraj", email:"nagaraj@dilabs.in",password:"12345"
+}
+let salt=bcrypt.genSaltSync(10);
+let new_password= bcrypt.hashSync(user.password,salt);
+let new_user={...user,password:new_password};
+
+console.log(new_user);
+
+let result=bcrypt.compareSync("12345",new_password)
+if(result){
+    console.log("Login Successfully")
+}else{
+    console.log("Login Failed")
+}
+
 
